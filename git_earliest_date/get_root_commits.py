ea9c707707c105d99e862ca8b98eba658a0ea5cc
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-10-25 23:00:00 +0300

    Add the `get_root_commits()` function

---
+++ git_earliest_date/get_root_commits.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+import pathlib
+
+import git
+import termcolor
+
+from . import entities
+from . import logger
+
+
+def get_root_commits(repo_dir: pathlib.Path) -> entities.RepoInfo:
+    formatted_repo_dir = termcolor.colored(str(repo_dir), "yellow")
+    logger.get_logger().debug(f"was found repo {formatted_repo_dir}")
+
+    repo = git.Repo(repo_dir)
+    if _is_repo_empty(repo):
+        logger.get_logger().warn(f"repo {formatted_repo_dir} is empty")
+
+        return entities.RepoInfo(repo_dir, [])
+
+    root_commits = _find_root_commits(repo)
+    return entities.RepoInfo(repo_dir, root_commits)
+
+
+def _is_repo_empty(repo: git.repo.base.Repo) -> bool:
+    return not repo.head.is_valid()
+
+
+def _find_root_commits(repo: git.repo.base.Repo) -> list[entities.CommitInfo]:
+    return list(
+        entities.CommitInfo._from_commit(root_commit)
+        for root_commit in repo.iter_commits(max_parents=0)
+    )
--- git_earliest_date/main.py
+++ git_earliest_date/main.py
@@ -3,6 +3,7 @@ import pathlib
 
 from . import logger
 from . import get_repo_dirs
+from . import get_root_commits
 
 
 def main() -> None:
@@ -11,8 +12,15 @@ def main() -> None:
 
         base_dir = pathlib.Path(sys.argv[1])
         repo_dirs = get_repo_dirs.get_repo_dirs(base_dir)
-        for repo_dir in repo_dirs:
-            logger.get_logger().debug(repo_dir)
+        repo_infos = (
+            get_root_commits.get_root_commits(repo_dir)
+            for repo_dir in repo_dirs
+        )
+        nonempty_repo_infos = (
+            repo_info for repo_info in repo_infos if not repo_info.is_empty_repo
+        )
+        for repo_info in nonempty_repo_infos:
+            logger.get_logger().debug(repo_info)
     except Exception as exception:
         logger.get_logger().error(exception)
         sys.exit(1)
