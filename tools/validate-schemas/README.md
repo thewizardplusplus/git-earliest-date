bb6626df3ed69ce94498054fd79d36a159bbff13
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-11-02 23:00:00 +0300

    Describe the tool for validating the JSON Schema of the result data

---
+++ tools/validate-schemas/README.md
@@ -0,0 +1,16 @@
+# validate-schemas
+
+The utility for validating result data by its JSON Schema.
+
+## Usage
+
+```
+$ validate_schemas.bash -h | --help
+$ validate_schemas.bash
+```
+
+Stdin: result data in the JSON format.
+
+Options:
+
+- `-h`, `--help` &mdash; show the help message and exit.
--- tools/validate-schemas/validate_schemas.bash
+++ tools/validate-schemas/validate_schemas.bash
@@ -22,6 +22,6 @@ declare -r script_dir="$(dirname "$0")"
 npx ajv-cli@5.0.0 validate \
   --spec "draft2020" \
   --strict true \
-  -s "$script_dir/../docs/schemas/repo_info_group.schema.json" \
-  -r "$script_dir/../docs/schemas/{repo_info,commit_info,person_info}.schema.json" \
+  -s "$script_dir/../../docs/schemas/repo_info_group.schema.json" \
+  -r "$script_dir/../../docs/schemas/{repo_info,commit_info,person_info}.schema.json" \
   -d "$data_file"
